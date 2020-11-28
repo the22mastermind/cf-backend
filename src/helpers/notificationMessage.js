a72911170dde58c9f2b7f6acd2b2734cd02a50e5
the22mastermind <bertrandmasabo22@gmail.com>
2020-11-29 00:17:45 +0200

    bg(rider-update-order):increase-cc-complexity

--- src/helpers/notificationMessage.js
+++ src/helpers/notificationMessage.js
@@ -13,30 +13,6 @@ const notificationMessageBuilder = async (messageType, data) => {
     title: 'Thank you!',
     body: 'Thank you for ordering with Chow Food! See you soon for another order',
   };
-  // if (messageType === 'processing') {
-  //   notificationData = {
-  //     title: `Your order #${orderId} has been accepted!`,
-  //     body: `Hi, I'm ${riderName}. I will be delivering your order today.`,
-  //   };
-  // }
-  // if (messageType === 'ontheway') {
-  //   notificationData = {
-  //     title: 'Heads up!',
-  //     body: `Rider ${riderName} is on the way with your order #${orderId}`,
-  //   };
-  // }
-  // if (messageType === 'arrived') {
-  //   notificationData = {
-  //     title: 'Heads up!',
-  //     body: `Rider ${riderName} has arrived at your location. Enjoy!`,
-  //   };
-  // }
-  // if (messageType === 'completed') {
-  //   notificationData = {
-  //     title: 'Thank you!',
-  //     body: 'Thank you for ordering with Chow Food! See you soon for another order',
-  //   };
-  // }
   return notificationData;
 };
 
