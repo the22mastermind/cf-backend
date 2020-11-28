7da6f48f1993c324edb9042865acdc960c6d24fe
the22mastermind <bertrandmasabo22@gmail.com>
2020-11-29 00:09:12 +0200

    bg(rider-update-order):refactor-notification-message-helper-ternary

--- src/helpers/notificationMessage.js
+++ src/helpers/notificationMessage.js
@@ -1,30 +1,42 @@
 const notificationMessageBuilder = async (messageType, data) => {
   const { orderId, riderName } = data;
-  let notificationData = null;
-  if (messageType === 'processing') {
-    notificationData = {
-      title: `Your order #${orderId} has been accepted!`,
-      body: `Hi, I'm ${riderName}. I will be delivering your order today.`,
-    };
-  }
-  if (messageType === 'ontheway') {
-    notificationData = {
-      title: 'Heads up!',
-      body: `Rider ${riderName} is on the way with your order #${orderId}`,
-    };
-  }
-  if (messageType === 'arrived') {
-    notificationData = {
-      title: 'Heads up!',
-      body: `Rider ${riderName} has arrived at your location. Enjoy!`,
-    };
-  }
-  if (messageType === 'completed') {
-    notificationData = {
-      title: 'Thank you!',
-      body: 'Thank you for ordering with Chow Food! See you soon for another order',
-    };
-  }
+  const notificationData = messageType === 'processing' ? {
+    title: `Your order #${orderId} has been accepted!`,
+    body: `Hi, I'm ${riderName}. I will be delivering your order today.`,
+  } : messageType === 'ontheway' ? {
+    title: 'Heads up!',
+    body: `Rider ${riderName} is on the way with your order #${orderId}`,
+  } : messageType === 'arrived' ? {
+    title: 'Heads up!',
+    body: `Rider ${riderName} has arrived at your location. Enjoy!`,
+  } : {
+    title: 'Thank you!',
+    body: 'Thank you for ordering with Chow Food! See you soon for another order',
+  };
+  // if (messageType === 'processing') {
+  //   notificationData = {
+  //     title: `Your order #${orderId} has been accepted!`,
+  //     body: `Hi, I'm ${riderName}. I will be delivering your order today.`,
+  //   };
+  // }
+  // if (messageType === 'ontheway') {
+  //   notificationData = {
+  //     title: 'Heads up!',
+  //     body: `Rider ${riderName} is on the way with your order #${orderId}`,
+  //   };
+  // }
+  // if (messageType === 'arrived') {
+  //   notificationData = {
+  //     title: 'Heads up!',
+  //     body: `Rider ${riderName} has arrived at your location. Enjoy!`,
+  //   };
+  // }
+  // if (messageType === 'completed') {
+  //   notificationData = {
+  //     title: 'Thank you!',
+  //     body: 'Thank you for ordering with Chow Food! See you soon for another order',
+  //   };
+  // }
   return notificationData;
 };
 
